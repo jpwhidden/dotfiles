012741ecc1527eef334c697fb2afa5e336e459b7
JP Whidden <john@whidden.net>
2017-09-29 18:20:02 -0600

    Add init script for raspbian

---
+++ init/10_rasbian.sh
@@ -0,0 +1,10 @@
+# Rasbian-only stuff. Abort if not Rasbian.
+#[[ `grep "^ID=" /etc/os-release | cut -d'=' -f2` == raspbian ]] || return 1
+
+# Update APT.
+e_header "Updating APT cache"
+sudo apt-get -qq update
+
+# Install APT packages.
+e_header "Installing APT packages"
+sudo apt-get install git-core git-gui tree nmap htop tmux mdadm lvm2 vim arduino
