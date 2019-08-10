f6265dea53868dc308102a2d1ac8b09d12a3d362
John Whidden <john@whidden.net>
2019-08-09 21:48:36 -0600

    Update raspbian init, ssh config, and tmux config

--- init/10_rasbian.sh
+++ init/10_rasbian.sh
@@ -1,13 +1,18 @@
 # Rasbian-only stuff. Abort if not Rasbian.
 #[[ `grep "^ID=" /etc/os-release | cut -d'=' -f2` == raspbian ]] || return 1
 
+# Install uv4l repo:
+e_header "Setting up uv4l repo"
+curl http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc | sudo apt-key add -
+echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ jessie main" | sudo tee -a /etc/apt/sources.list
+
 # Update APT.
 e_header "Updating APT cache"
 sudo apt-get -qq update
 
 # Install APT packages.
 e_header "Installing APT packages"
-sudo apt-get install git-core git-gui tree nmap htop tmux mdadm lvm2 vim arduino obconf libjpeg-dev libv4l-dev autoconf automake android-tools-adb
+sudo apt-get install git-core git-gui tree nmap htop tmux mdadm lvm2 vim arduino obconf libjpeg-dev libv4l-dev autoconf automake android-tools-adb uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-webrtc iceweasel autossh screen exfat-fuse exfat-utils meld jq
 
 # Install pip packages
 sudo pip install awscli
