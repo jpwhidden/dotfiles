3f025ad0b10ec84efd744bf311234d0dd27bd5a5
JP Whidden <john@whidden.net>
2019-09-25 14:29:08 -0600

    Rasbian init changes. Remove uv4l repo from init. Update initial packages to install.

--- init/10_rasbian.sh
+++ init/10_rasbian.sh
@@ -2,17 +2,17 @@
 #[[ `grep "^ID=" /etc/os-release | cut -d'=' -f2` == raspbian ]] || return 1
 
 # Install uv4l repo:
-e_header "Setting up uv4l repo"
-curl http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc | sudo apt-key add -
-echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ jessie main" | sudo tee -a /etc/apt/sources.list
+#e_header "Setting up uv4l repo"
+#curl http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc | sudo apt-key add -
+#echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ jessie main" | sudo tee -a /etc/apt/sources.list
 
 # Update APT.
-e_header "Updating APT cache"
-sudo apt-get -qq update
+# e_header "Updating APT cache"
+# sudo apt-get -qq update
 
 # Install APT packages.
 e_header "Installing APT packages"
-sudo apt-get install git-core git-gui tree nmap htop tmux mdadm lvm2 vim arduino obconf libjpeg-dev libv4l-dev autoconf automake android-tools-adb uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-webrtc iceweasel autossh screen exfat-fuse exfat-utils meld jq
+sudo apt install git git-gui tree nmap htop tmux mdadm lvm2 vim-nox arduino obconf libjpeg-dev libv4l-dev autoconf automake android-tools-adb autossh screen exfat-fuse exfat-utils meld jq samba ntp
 
 # Install pip packages
 sudo pip install awscli
